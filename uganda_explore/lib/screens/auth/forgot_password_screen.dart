6844acb3537b5d16f5c9b4350150e73471554f65
blaisedavis21 <blaisedavis207@gmail.com>
2025-06-30 16:45:02 +0300

    added form and global key for password verification

--- uganda_explore/lib/screens/auth/forgot_password_screen.dart
+++ uganda_explore/lib/screens/auth/forgot_password_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ForgotPasswordScreen extends StatelessWidget {
-  const ForgotPasswordScreen({super.key});
+  ForgotPasswordScreen({super.key});
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -13,55 +17,98 @@ class ForgotPasswordScreen extends StatelessWidget {
           gradient: RadialGradient(
             center: Alignment.center,
             radius: 1.0,
-            colors: [Color(0xFF0C0F0A), Color(0xFF1EF813)],
+            colors: [
+              Color(0xFF0C0F0A),
+              Color(0xFF1EF813),
+            ],
             stops: [0.03, 0.63],
           ),
         ),
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 60),
-              Center(
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  decoration: const BoxDecoration(shape: BoxShape.circle),
-                  child: Image.asset(
-                    'assets/logo/blacklogo.png',
-                    fit: BoxFit.contain,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const SizedBox(height: 60),
+                Center(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                    ),
+                    child: Image.asset(
+                      'assets/logo/blacklogo.png',
+                      fit: BoxFit.contain,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                "Let's get you\n sorted!",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'Outfit',
-                  fontSize: 37,
-                  fontWeight: FontWeight.w900,
-                  color: Colors.black,
+                const SizedBox(height: 20),
+                const Text(
+                  "Let's get you\n sorted!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Outfit',
+                    fontSize: 37,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.black,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-          Container(
-            width: 490,
-            padding: const EdgeInsets.only(left: 4, right: 4, bottom: 0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(40),
-              ),
-            ),
-            child: Column(
-              children: const [
-                SizedBox(height: 30),
-                // Form elements will go here
+                const SizedBox(height: 20),
+                Container(
+                  width: 490,
+                  padding: const EdgeInsets.only(left: 4, right: 4, bottom: 0),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(40),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 30),
+                      const PasswordResetIcon(),
+                      const SizedBox(height: 20),
+                      const Text(
+                        "Reset Your Password",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Inter',
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        "Create a new password for your\naccount below.",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'Poppins',
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 30),
+                      NewPasswordField(controller: _newPasswordController),
+                      const SizedBox(height: 20),
+                      ConfirmPasswordField(controller: _confirmPasswordController),
+                      const SizedBox(height: 30),
+                      ChangePasswordButton(
+                        formKey: _formKey,
+                        newPasswordController: _newPasswordController,
+                        confirmPasswordController: _confirmPasswordController,
+                      ),
+                      const SizedBox(height: 20),
+                      const BackToSignInLink(),
+                      const SizedBox(height: 30),
+                    ],
+                  ),
+                ),
               ],
             ),
-          ),
-          
-            ],
           ),
         ),
       ),
@@ -91,7 +138,8 @@ class PasswordResetIcon extends StatelessWidget {
 }
 
 class NewPasswordField extends StatefulWidget {
-  const NewPasswordField({super.key});
+  final TextEditingController controller;
+  const NewPasswordField({required this.controller, super.key});
 
   @override
   State<NewPasswordField> createState() => _NewPasswordFieldState();
@@ -99,28 +147,144 @@ class NewPasswordField extends StatefulWidget {
 
 class _NewPasswordFieldState extends State<NewPasswordField> {
   bool _isObscured = true;
-  final _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 450,
-      child: TextFormField(
-        controller: _controller,
-        obscureText: _isObscured,
-        decoration: InputDecoration(
-          labelText: 'New Password',
-          suffixIcon: IconButton(
-            icon: Icon(
-              _isObscured ? Icons.visibility_off : Icons.visibility,
-              color: Colors.black54,
+    return Center(
+      child: SizedBox(
+        width: 450,
+        child: TextFormField(
+          controller: widget.controller,
+          obscureText: _isObscured,
+          decoration: InputDecoration(
+            labelText: 'New Password',
+            labelStyle: const TextStyle(
+              color: Colors.black,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
             ),
-            onPressed: () {
-              setState(() {
-                _isObscured = !_isObscured;
-              });
-            },
+            hintText: 'Enter Your New Password',
+            hintStyle: const TextStyle(
+              color: Colors.black54,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w400,
+              fontSize: 14,
+            ),
+            filled: true,
+            fillColor: Colors.white,
+            prefixIcon: const Icon(Icons.lock, color: Colors.black),
+            suffixIcon: IconButton(
+              icon: Icon(
+                _isObscured ? Icons.visibility_off : Icons.visibility,
+                color: Colors.black54,
+              ),
+              onPressed: () {
+                setState(() {
+                  _isObscured = !_isObscured;
+                });
+              },
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(
+                color: Color(0xFF1EF813),
+                width: 1,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(
+                color: Color(0xFF1EF813),
+                width: 2,
+              ),
+            ),
+            contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
           ),
+          style: const TextStyle(
+            color: Colors.black,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w400,
+            fontSize: 15,
+          ),
+          cursorColor: Color(0xFF1EF813),
+        ),
+      ),
+    );
+  }
+}
+
+class ConfirmPasswordField extends StatefulWidget {
+  final TextEditingController controller;
+  const ConfirmPasswordField({required this.controller, super.key});
+
+  @override
+  State<ConfirmPasswordField> createState() => _ConfirmPasswordFieldState();
+}
+
+class _ConfirmPasswordFieldState extends State<ConfirmPasswordField> {
+  bool _isObscured = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: 450,
+        child: TextFormField(
+          controller: widget.controller,
+          obscureText: _isObscured,
+          decoration: InputDecoration(
+            labelText: 'Confirm Password',
+            labelStyle: const TextStyle(
+              color: Colors.black,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+            ),
+            hintText: 'Confirm Your Password',
+            hintStyle: const TextStyle(
+              color: Colors.black54,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w400,
+              fontSize: 14,
+            ),
+            filled: true,
+            fillColor: Colors.white,
+            prefixIcon: const Icon(Icons.lock_outline, color: Colors.black),
+            suffixIcon: IconButton(
+              icon: Icon(
+                _isObscured ? Icons.visibility_off : Icons.visibility,
+                color: Colors.black54,
+              ),
+              onPressed: () {
+                setState(() {
+                  _isObscured = !_isObscured;
+                });
+              },
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(
+                color: Color(0xFF1EF813),
+                width: 1,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(
+                color: Color(0xFF1EF813),
+                width: 2,
+              ),
+            ),
+            contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+          ),
+          style: const TextStyle(
+            color: Colors.black,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w400,
+            fontSize: 15,
+          ),
+          cursorColor: Color(0xFF1EF813),
         ),
       ),
     );
@@ -128,46 +292,64 @@ class _NewPasswordFieldState extends State<NewPasswordField> {
 }
 
 class ChangePasswordButton extends StatelessWidget {
-  const ChangePasswordButton({super.key});
+  final GlobalKey<FormState> formKey;
+  final TextEditingController newPasswordController;
+  final TextEditingController confirmPasswordController;
 
-  void _onPressed(BuildContext context) {
+  const ChangePasswordButton({
+    required this.formKey,
+    required this.newPasswordController,
+    required this.confirmPasswordController,
+    super.key,
+  });
+
+  void _onChangePasswordPressed(BuildContext context) {
+    // Validation logic can be added in the next part
     showDialog(
       context: context,
-      builder: (_) => AlertDialog(
-        title: const Text("Success"),
-        content: const Text("Your password has been changed successfully!"),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-              Navigator.pushReplacementNamed(context, '/signin');
-            },
-            child: const Text("OK"),
-          ),
-        ],
-      ),
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Success'),
+          content: const Text('Your password has been changed successfully!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close dialog
+                Navigator.pushReplacementNamed(context, '/signin'); // Navigate to sign in
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => _onPressed(context),
-      child: Container(
+    return Center(
+      child: SizedBox(
         width: 450,
-        height: 50,
-        decoration: BoxDecoration(
-          gradient: const LinearGradient(
-            colors: [Color.fromARGB(255, 47, 44, 44), Color(0xFF1EF813)],
-          ),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        alignment: Alignment.center,
-        child: const Text(
-          'Change Password',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+        child: GestureDetector(
+          onTap: () => _onChangePasswordPressed(context),
+          child: Container(
+            height: 50,
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                colors: [Color.fromARGB(255, 47, 44, 44), Color(0xFF1EF813)],
+              ),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            alignment: Alignment.center,
+            child: const Text(
+              'Change Password',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ),
       ),
@@ -180,13 +362,20 @@ class BackToSignInLink extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => Navigator.pop(context),
-      child: const Text(
-        'Back to Sign In',
-        style: TextStyle(
-          color: Color(0xFF0F7709),
-          fontWeight: FontWeight.w600,
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: const Text(
+          'Back to Sign In',
+          style: TextStyle(
+            color: Color(0xFF0F7709),
+            fontSize: 16,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w600,
+          ),
+          textAlign: TextAlign.center,
         ),
       ),
     );
