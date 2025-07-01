3ed71326cab626b27fd6337ffdee45b26928c1b6
Alvin11David <alvin69david@gmail.com>
2025-07-01 09:03:54 +0300

    changed the name of forgot password

--- uganda_explore/lib/screens/auth/change_password_screen.dart
+++ uganda_explore/lib/screens/auth/change_password_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ForgotPasswordScreen extends StatelessWidget {
-  const ForgotPasswordScreen({super.key});
+class ChangePasswordScreen extends StatelessWidget {
+  const ChangePasswordScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
