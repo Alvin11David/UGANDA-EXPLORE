e877595eb2a8fa148d418668e36c8270c57b6ae4
blaisedavis21 <blaisedavis207@gmail.com>
2025-07-02 10:30:01 +0300

    added the navigation buttons

--- uganda_explore/lib/screens/error/error_screen.dart
+++ uganda_explore/lib/screens/error/error_screen.dart
@@ -2,67 +2,73 @@ import 'package:flutter/material.dart';
 
 class PageNotFoundScreen extends StatelessWidget {
   const PageNotFoundScreen({super.key});
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            leading: IconButton(
-              icon: const Icon(Icons.arrow_back),
-              onPressed: () => Navigator.of(context).pop(),
-            ) ,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.green,
+            child: Center(
+              child: Icon(
+                Icons.signal_wifi_off,
+                size: 100,
+                color: Colors.black,
+              ),
+            ),
           ),
-          body: Stack(
-            children: [
-              Container(
-                color: Colors.green,
-                child: Center(
-                  child: Icon(
-                    Icons.signal_wifi_off,
-                    size: 100,
-                    color: Colors.black,
+          Center(
+            child: Card(
+              margin: EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.green,
+                    child: Icon(
+                      Icons.question_mark,
+                      size: 50,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-              ),
-              Center(
-                child: Card(
-                  margin: EdgeInsets.all(20),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
+                  SizedBox(height: 20),
+                  Text(
+                    'Oops! Page Not Found',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'We couldn\'t find the page you were looking for.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      CircleAvatar(
-                        radius: 50,
-                        backgroundColor: Colors.green,
-                        child: Icon(
-                          Icons.question_mark,
-                          size: 50,
-                          color: Colors.white,
-                        )
+                      ElevatedButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: Text('Go Back'),
                       ),
-                      SizedBox(height: 20),
-                      Text(
-                        'Oops! Page Not Found',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      ElevatedButton(
+                        onPressed: () => Navigator.of(context).pushNamed('/home'),
+                        child: Text('Home'),
                       ),
-                      SizedBox(height: 10),
-                      Text(
-                        'We couldn\'t find the page you were looking for.',
-                        textAlign: TextAlign.center,
-                        style: TextStyle( 
-                          fontSize: 16,
-                          color: Colors.grey[700],
-                          
-                        ),
-
-                      ),
-                    ],),
-                ),
+                    ],
+                  ),
+                ],
               ),
-              ]
-              ),
-        );
-      }
-
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
