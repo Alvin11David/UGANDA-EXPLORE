21e4091ad505f62af1a0b98bc418f8a3afa4d17b
blaisedavis21 <blaisedavis207@gmail.com>
2025-07-04 08:55:34 +0300

    Set up AppThemeScreen with state, background image, and scrollable SafeArea layout

--- uganda_explore/lib/apptheme/apptheme.dart
+++ uganda_explore/lib/apptheme/apptheme.dart
@@ -1,7 +1,15 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
-class AppThemeScreen extends StatelessWidget {
+class AppThemeScreen extends StatefulWidget {
   const AppThemeScreen({super.key});
+
+  @override
+  State<AppThemeScreen> createState() => _AppThemeScreenState();
+}
+
+class _AppThemeScreenState extends State<AppThemeScreen> {
+  bool isDarkMode = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +20,30 @@ class AppThemeScreen extends StatelessWidget {
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage(
-              'assets/images/vertical-shot-some-beautiful-trees-sun-setting-background.jpg'
-              ), 
+              'assets/images/vertical-shot-some-beautiful-trees-sun-setting-background.jpg',
+            ),
             fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: constraints.maxHeight,
+                  ),
+                  child: IntrinsicHeight(
+                    child: Column(
+                      children: [
+                        // Future widgets go here (header, theme card, etc.)
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
           ),
         ),
       ),
