dbc1575c0a84b8ea7baa864424fe1f03f7584fc1
denistendo <ssebyaladenis18@gmail.com>
2025-07-04 12:36:29 +0300

    started on the edit profile screen

--- uganda_explore/lib/main.dart
+++ uganda_explore/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:uganda_explore/firebase_options.dart';
+import 'package:uganda_explore/screens/auth/change_password_screen.dart';
 
 // Auth Screens
 import 'package:uganda_explore/screens/auth/forgot_password_screen.dart';
@@ -11,18 +12,15 @@ import 'package:uganda_explore/screens/auth/sign_in_screen.dart';
 import 'package:uganda_explore/screens/splash/onboarding_screen1.dart';
 import 'package:uganda_explore/screens/splash/onboarding_screen2.dart';
 import 'package:uganda_explore/screens/splash/onboarding_screen3.dart';
-import 'package:uganda_explore/screens/splash/splash_screen.dart';
 
 // Other Screens
 import 'package:uganda_explore/screens/home/home_screen.dart';
 import 'package:uganda_explore/screens/profile/profile_screen.dart';
-import 'package:uganda_explore/screens/error/error_screen.dart';
+import 'package:uganda_explore/screens/splash/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -44,13 +42,12 @@ class MyApp extends StatelessWidget {
         '/onboarding_screen2': (context) => const OnboardingScreen2(),
         '/onboarding_screen3': (context) => const OnboardingScreen3(),
         '/forgot_password': (context) => const ForgotPasswordScreen(),
-        '/otp': (context) => const OTPScreen(),
+        '/otp': (context) => const OtpScreen(email: ''),
         '/home': (context) => const HomeScreen(),
         '/profile': (context) => const ProfileScreen(),
-        '/error': (context) => const ErrorScreen(),
-      ],
-      // Set the initial screen you want to load
-      home: const ForgotPasswordScreen(), // Change to SplashScreen(), SignInScreen(), etc. as needed
+      },
+
+      home: const SplashScreen(),
     );
   }
 }
