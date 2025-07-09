c82ea2251431de5149440d58e5d71f7d00919cff
denistendo <ssebyaladenis18@gmail.com>
2025-07-09 20:52:24 +0300

    added navigation logic for nav bar

--- uganda_explore/lib/screens/profile/settings_screen.dart
+++ uganda_explore/lib/screens/profile/settings_screen.dart
@@ -7,3 +7,19 @@ class SettingsScreen extends StatefulWidget {
   @override
   State<SettingsScreen> createState() => _SettingsScreenState();
 }
+class _SettingsScreenState extends State<SettingsScreen> {
+  int _selectedIndex = 2;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    if (index == 0) {
+      Navigator.pushReplacementNamed(context, '/home');
+    } else if (index == 1) {
+      Navigator.pushReplacementNamed(context, '/profile');
+    } else if (index == 3) {
+      Navigator.pushReplacementNamed(context, '/map');
+    }
+    // Add more navigation logic for other indices if needed
+  }
