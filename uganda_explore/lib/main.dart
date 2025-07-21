f481abbd69c09cd78d8600b37bbe69ee6887c6f7
denistendo <ssebyaladenis18@gmail.com>
2025-07-21 18:02:38 +0300

    pulled from main

--- uganda_explore/lib/main.dart
+++ uganda_explore/lib/main.dart
@@ -89,6 +89,7 @@ class MyApp extends StatelessWidget {
             '/admin_dashboard': (context) => const AdminDashboard(),
             '/termsandprivacy': (context) => const TermsPrivacyScreen(),
           },
+          
           home: const HomeScreen(),
         );
       },
